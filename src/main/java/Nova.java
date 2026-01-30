249fdc07f64dad19c82b7ad1ffb8f839d7a05d1a
Saksham Jain <jain.saksham2006@gmail.com>
2026-01-30 13:58:22 +0800

    Completed and added the feature of add and list (Level-2)

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Nova {
     public static void main(String[] args) {
@@ -6,18 +7,23 @@ public class Nova {
 
         System.out.println("Hello! I'm Nova");
         System.out.println("What can I do for you?");
+        ArrayList<String> taskList = new ArrayList<>();
 
         while(true){
-            if(!scan.hasNextLine()){
-                break;
-            }
+
             String userInput = scan.nextLine();
 
             if(userInput.equalsIgnoreCase("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+
+            }else if(userInput.equalsIgnoreCase("list")){
+                for(int i=0; i<taskList.size();i++){
+                    System.out.println((i+1) + ". "+ taskList.get(i));
+                }
             }else{
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                taskList.add(userInput);
             }
 
         }
