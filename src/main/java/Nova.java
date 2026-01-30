5096017733f89314f7cd68244db5e857efa0ac40
Saksham Jain <jain.saksham2006@gmail.com>
2026-01-30 12:29:43 +0800

    updates of code for level 1

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 public class Nova {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello! I'm Nova");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true){
+            if(!scan.hasNextLine()){
+                break;
+            }
+            String userInput = scan.nextLine();
+
+            if(userInput.equalsIgnoreCase("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }else{
+                System.out.println(userInput);
+            }
+
+        }
+
+        scan.close();
     }
 }
