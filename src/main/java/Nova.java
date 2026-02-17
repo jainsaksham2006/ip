b93c1add49a0af184a7757693044f4488fe3bdb3
Saksham Jain <jain.saksham2006@gmail.com>
2026-02-17 17:01:53 +0800

    Implemented Exceptions

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -49,12 +49,17 @@ public class Nova {
                     System.out.println("Please specify which task number to mark.");
                     continue;
                 }
-
-                int index = Integer.parseInt(userCommand[1]) - 1;
-                Task task = taskList.get(index);
-                task.markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + task);
+                try{
+                    int index = Integer.parseInt(userCommand[1]) - 1;
+                    Task task = taskList.get(index);
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + task);
+                }catch(NumberFormatException e){
+                    System.out.println("Task number must be a valid number.");
+                }catch( IndexOutOfBoundsException e){
+                    System.out.println("That task number does not exist.");
+                }
 
             } else if (userCommand[0].equalsIgnoreCase("unmark")) {
 
@@ -63,12 +68,19 @@ public class Nova {
                     continue;
                 }
 
-                int index = Integer.parseInt(userCommand[1]) - 1;
-                Task task = taskList.get(index);
-                task.markAsNotDone();
+                try{
+                    int index = Integer.parseInt(userCommand[1]) - 1;
+                    Task task = taskList.get(index);
+                    task.markAsNotDone();
 
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + task);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + task);
+                }catch(NumberFormatException e){
+                    System.out.println("Task number must be a valid number.");
+                }catch( IndexOutOfBoundsException e){
+                    System.out.println("That task number does not exist.");
+                }
+
             } else if (userCommand[0].equalsIgnoreCase("todo")) {
                 String description = userInput.substring(5).trim();
                 if(description.isEmpty()){
@@ -79,35 +91,64 @@ public class Nova {
                 taskList.add(task);
                 printTaskAdded(task, taskList);
             } else if (userCommand[0].equalsIgnoreCase("deadline") && userCommand.length > 1) {
+                try{
 
-                if (!userInput.contains("/by")) {
-                    System.out.println("A deadline needs a /by date. Example: deadline homework /by Sunday");
-                    continue;
+                    if (!userInput.contains("/by")) {
+                        System.out.println("A deadline needs a /by date. Example: deadline homework /by Sunday");
+                        continue;
+                    }
+
+
+                    String fullCommand = String.join(" ", userCommand);
+                    String[] parts = fullCommand.split("/by");
+                    String description = parts[0].replace("deadline", "").trim();
+                    String by = parts[1].trim();;
+
+                    if(by.isEmpty()){
+                        System.out.println("Deadline date cannot be empty.");
+                        continue;
+                    }
+
+                    if(description.isEmpty()){
+                        System.out.println("Deadline description cannot be empty.");
+                        continue;
+                    }
+                    Task task = new Deadline(description, by);
+                    taskList.add(task);
+                    printTaskAdded(task, taskList);
+                }catch(Exception e){
+                    System.out.println("Invalid Deadline Format.");
                 }
 
-                String fullCommand = String.join(" ", userCommand);
-                String[] parts = fullCommand.split("/by");
-                String description = parts[0].replace("deadline", "").trim();
-                String by = parts[1].trim();;
-                Task task = new Deadline(description, by);
-                taskList.add(task);
-                printTaskAdded(task, taskList);
 
             } else if (userCommand[0].equalsIgnoreCase("event") && userCommand.length > 1) {
+                try{
+                    if (!userInput.contains("/from") || !userInput.contains("/to")) {
+                        System.out.println("An event needs /from and /to times.");
+                        continue;
+                    }
 
-                if (!userInput.contains("/from") || !userInput.contains("/to")) {
-                    System.out.println("An event needs /from and /to times.");
-                    continue;
+                    String fullCommand = String.join(" ", userCommand);
+                    String[] parts = fullCommand.split("/from");
+                    String description = parts[0].replace("event", "").trim();
+                    String[] duration = parts[1].split("/to", 2);
+                    String startTime = duration[0].trim();;
+                    String endTime = duration[1].trim();;
+
+                    if(startTime.isEmpty() || endTime.isEmpty()){
+                        System.out.println("Event time cannot be empty.");
+                        continue;
+                    }
+                    if(description.isEmpty()){
+                        System.out.println("Event description cannot be empty.");
+                        continue;
+                    }
+                    Task task = new Event(description, startTime, endTime);
+                    taskList.add(task);
+                    printTaskAdded(task, taskList);
+                }catch(Exception e){
+                    System.out.println("Invalid Event Format.");
                 }
-                String fullCommand = String.join(" ", userCommand);
-                String[] parts = fullCommand.split("/from");
-                String description = parts[0].replace("event", "").trim();
-                String[] duration = parts[1].split("/to", 2);
-                String startTime = duration[0].trim();;
-                String endTime = duration[1].trim();;
-                Task task = new Event(description, startTime, endTime);
-                taskList.add(task);
-                printTaskAdded(task, taskList);
 
             } else {
                 System.out.println("I don't recognize that command.");
